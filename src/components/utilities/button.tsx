dc4f06473b497fd4b57a7f98c660d34bb7f2eb4e
RobertKilkenny <robert.kilkenny7@gmail.com>
2023-11-09 20:03:04 -0500

    Added more buttons and lessened the amount of needed variables

--- src/components/utilities/button.tsx
+++ src/components/utilities/button.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { Children, useState } from "react";
 import "./button.css";
 import { Link } from "react-router-dom";
 
@@ -7,16 +7,21 @@ const SIZES = ["btn-medium", "btn-large"];
 const COLORS: Map<string, [string, string, string, string]> = new Map([
   ["green", ["#22AB22", "#000000", "#00CC00", "#FFFFFF"]],
   ["blue", ["#0000C6", "#DFCC00", "#0000EE", "#FFFFFF"]],
+  ["white", ["#FFFFFF", "#000000", "#AAAAAA", "#000000"]],
+  ["black", ["#000000", "#FFFFFF", "#000000", "#FF60FF"]],
+  ["UF", ["#004BFF", "#FF6700", "#111288", "#F88F03"]],
+  ["greygreen", ["#666666", "#00EF00", "#111111", "#32BC32"]],
 ]);
 
 interface Props {
   _routing: string;
-  _buttonStyle: string;
   _buttonSize: string;
+
+  _usePalette?: boolean;
   children?: React.ReactNode;
   _onClick?: () => void;
+  _buttonStyle?: string;
   _buttonColor?: string;
-  _usePalette?: boolean;
   _palette?: [string, string, string, string];
 }
 
@@ -27,7 +32,7 @@ const Button = ({
   _buttonSize,
   _buttonColor,
   children,
-  _usePalette,
+  _usePalette = false,
   _palette,
 }: Props) => {
   const [isHovered, setIsHovered] = useState(false);
@@ -39,9 +44,9 @@ const Button = ({
     setIsHovered(false);
   };
 
-  const checkButtonStyle = STYLES.includes(_buttonStyle)
+  const checkButtonStyle = STYLES.includes(_buttonStyle || "")
     ? _buttonStyle
-    : STYLES[0];
+    : "";
   const checkButtonSize = SIZES.includes(_buttonSize) ? _buttonSize : SIZES[0];
   const colorCheck = typeof _buttonColor == "string" ? _buttonColor : "";
   const _className = `btn ${checkButtonStyle} ${checkButtonSize} `;
@@ -50,7 +55,7 @@ const Button = ({
     COLORS.get(colorCheck)?.[0] || _usePalette
       ? isHovered
         ? {
-            border: "1px solid",
+            border: "2px solid",
 
             backgroundColor: _usePalette
               ? _palette?.[2]
@@ -63,7 +68,7 @@ const Button = ({
               : COLORS.get(colorCheck)?.[3],
           }
         : {
-            border: "1px solid",
+            border: "2px solid",
 
             backgroundColor: _usePalette
               ? _palette?.[0]
@@ -78,6 +83,10 @@ const Button = ({
       : {};
 
   //console.log(`value ${_buttonColor} gives styling ${_styling}`);
+  if (!_usePalette && colorCheck == "" && checkButtonStyle == "") {
+    var report = `A button does not have any type of styling!! It the following children: ${children}`;
+    console.log(report);
+  }
 
   const result = (
     <Link to={_routing} className={_buttonStyle}>
