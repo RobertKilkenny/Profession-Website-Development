ac4da6cb18ddf542dd09bbc92525958902238ee1
RobertKilkenny <robert.kilkenny7@gmail.com>
2023-11-21 20:09:36 -0500

    Added content to the contact me page (in progress)

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -1,3 +1,64 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Separator } from "./ui/separator";
+
+interface Social {
+  id: "";
+  image_location: "";
+  link: "";
+}
+
+async function getSocialList() {
+  const result = await fetch("/data/social.json", {
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+  });
+  const json = await result.json();
+  return json["social"];
+}
+
 export default function ContactMe() {
-  return <div>ContactMe</div>;
+  const [data, setData] = useState<Social[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await getSocialList();
+        setData(result);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="page-content-holder">
+      <h1>Have Something to Say?</h1>
+      <Separator />
+      <div className="main-content-holder">
+        <p className="main-content-text">
+          You can find me here on my different pages
+        </p>
+
+        <div className="object-container-horizon space-x-10">
+          {data.map((social) => (
+            <Link to={social.link} key={social.id}>
+              <img
+                className="secondary-content-image-sq"
+                src={social.image_location}
+                color="accent"
+              />
+            </Link>
+          ))}
+        </div>
+        <p className="main-content-text">
+          or you can email me at <strong>robert.kilkenny7@gmail.com</strong>
+        </p>
+      </div>
+    </div>
+  );
 }
