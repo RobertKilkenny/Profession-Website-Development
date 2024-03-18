88301a63bde346060f326670815a8424aa574800
RobertKilkenny <73616441+RobertKilkenny@users.noreply.github.com>
2024-03-18 16:54:44 -0400

    Finished image holder for the time. Removed autocycling due to it causing too many bugs :(

--- src/utils/ImageCarousel.tsx
+++ src/utils/ImageCarousel.tsx
@@ -1,68 +1,52 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { ArrowBigLeftDash, ArrowBigRightDash } from 'lucide-react';
 import { Card, CardContent, CardFooter } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
-import { cycleImages } from './Custom functions/image-cycling';
-
-const TASK_TIMEOUT = 5000;
 
 interface ImageCarouselProps {
-  rootDir: string;
-  imagesToCycle: string[][];
+    rootDir: string;
+    imagesToCycle: string[][];
 }
 
-const ImageCarousel: React.FC<ImageCarouselProps> = ({ rootDir, imagesToCycle }) => {
+const ImageCarousel: React.FC<ImageCarouselProps> = ({ rootDir, imagesToCycle}) => {
   const [cycleIndex, setCycleIndex] = useState<number>(0);
-  const [isCyclingManually, setIsCyclingManually] = useState<boolean>(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsCyclingManually(false);
-    }, TASK_TIMEOUT);
-
-    return () => {
-      clearTimeout(timer);
-    };
-  }, [isCyclingManually]);
 
   const handleUserCycle = (shouldGoLeft: boolean) => {
-    setIsCyclingManually(true);
-    const newIndex = shouldGoLeft ? (cycleIndex === 0 ? imagesToCycle.length : cycleIndex - 1) : (cycleIndex === imagesToCycle.length - 1 ? 0 : cycleIndex + 1);
+    const newIndex = shouldGoLeft ? (cycleIndex === 0 ? imagesToCycle.length - 1 : cycleIndex - 1) : (cycleIndex === imagesToCycle.length - 1 ? 0 : cycleIndex + 1);
     setCycleIndex(newIndex);
   };
-
-  useEffect(() => {
-    if (cycleIndex < 1) {
-      setCycleIndex(1);
-      cycleImages(imagesToCycle.length, () => isCyclingManually, setCycleIndex, cycleIndex);
-    }
-  }, [cycleIndex, imagesToCycle, isCyclingManually]);
 
   if (!imagesToCycle || imagesToCycle.length < 1) return null;
 
   return (
-    <Card className="flex flex-col w-1/3 min-w-[350px]">
-      <CardContent className="flex flex-row items-center">
-        {imagesToCycle.length > 1 && (
-          <Button onClick={() => handleUserCycle(true)}>
-            <ArrowBigLeftDash className="w-10 h-10" />
+    <div className={`flex flex-col align-middle w-1/4 max-h-[25vm]`}>
+      <Card className="flex flex-col items-center aspect-[2] p-5 max-h-[20vm]">
+        <CardContent className="flex flex-row justify-center">
+          <img
+            className="project-card-image max-w-5/6 max-h-5/6"
+            src={imagesToCycle[cycleIndex] ? `/data/${rootDir}/${imagesToCycle[cycleIndex][0]}` : ''}
+            alt="Project Image Carousel"
+          />
+        </CardContent>
+        <CardFooter className='flex flex-col p-0 max-h-1/6'>
+          <p className="project-card-text">{imagesToCycle[cycleIndex] ? imagesToCycle[cycleIndex][1] : ''}</p>
+          <p className="project-card-text">{`Image ${cycleIndex + 1} of ${imagesToCycle.length}`}</p>
+        </CardFooter>
+      </Card>
+
+      {imagesToCycle.length > 1 && (
+        <div className='flex flex-row w-[100%] p-5 justify-between self-end'>
+          <Button className='flex m-1 w-1/4 aspect-[4/3]'
+            onClick={() => handleUserCycle(true)}>
+            <ArrowBigLeftDash />
           </Button>
-        )}
-        <img
-          className="project-card-image"
-          src={imagesToCycle[cycleIndex] ? `/data/${rootDir}/${imagesToCycle[cycleIndex][0]}` : ''}
-          alt="Project Image Carousel"
-        />
-        {imagesToCycle.length > 1 && (
-          <Button onClick={() => handleUserCycle(false)}>
-            <ArrowBigRightDash className="w-10 h-10" />
+          <Button className='flex m-1 w-1/4 aspect-[4/3]'
+            onClick={() => handleUserCycle(false)}>
+            <ArrowBigRightDash />
           </Button>
-        )}
-      </CardContent>
-      <CardFooter>
-        <p className="main-content-text">{imagesToCycle[cycleIndex] ? imagesToCycle[cycleIndex][1] : ''}</p>
-      </CardFooter>
-    </Card>
+        </div>
+      )}
+    </div>
   );
 };
 
