b7d57036b2709549e22ee10ec2744d5fcc692361
Robert Kilkenny <73616441+RobertKilkenny@users.noreply.github.com>
2024-05-05 23:11:18 -0400

    New checks using Husky and Prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,22 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'prettier',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
+    'react/react-in-jsx-scope': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'prettier': 'error',
   },
+  settings: {
+    react: {
+      version: 'detect',
+    }
+  }
 }
