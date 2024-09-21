9e8163ba96b4141d1002ea1ec46aedbb487c1474
Robert Kilkenny <robert.kilkenny7@gmail.com>
2024-09-20 23:12:52 -0400

    Adding the Random Wheel docs and some features

---
+++ src/utils/RouteToRandomWheel.tsx
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+const RouteToRandomWheel = () => {
+    const query = new URLSearchParams(useLocation().search);
+    const queryElements = query.get('elements');
+    const queryCheat = query.get('cheat');
+    let href = 'https://robertkilkenny.github.io/Random-Wheel/';
+    
+    if (queryElements) {
+      href = href + "?elements=" + queryElements;
+      if (queryCheat) {
+        href = href + "&cheat=" + queryCheat;
+      }
+    }
+
+    useEffect(() => {
+      window.location.href = href;
+    }, [href]);
+    
+    return <div>Redirecting to the Random Wheel...</div>;
+  };
+  
+
+export default RouteToRandomWheel;
