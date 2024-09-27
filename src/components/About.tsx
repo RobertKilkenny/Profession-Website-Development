8dc041f40f5f19aff442fe7a5ea98262d8803327
Robert Kilkenny <robert.kilkenny7@gmail.com>
2024-09-27 18:52:07 -0400

    refactor(about page): changed Bio to reflect myself and reword different sections and compress it

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -47,28 +47,27 @@ function About() {
         <div className="main-content-holder">
           <h2>My Story</h2>
           <p>
-            Hi, I am Robert Kilkenny, currently a student at the University of
-            Florida (UF) going into my senior year. I plan to graduate in
-            December of 2024 with a Bachelor's Degree in Computer Science and a
-            minor in Mathematics.
+            Hello! My name is Robert Kilkenny, and I’m a passionate programmer
+            with a love for creating various apps and websites to meet my
+            personal and professional needs. Many of these projects are
+            displayed <a href="https://kilkenny.dev/#/projects">here</a> for you
+            to explore. Throughout my journey, I’ve gained experience in
+            multiple programming languages and frameworks, from Python to C++
+            and JavaScript, with a focus on frontend development. However, I’ve
+            also worked on backend applications and database integration,
+            particularly through my coursework.
           </p>
           <p>
-            I run a club at UF about creating video games called "DevLUp" to
-            help students learn about coding principles like using GitHub,
-            Object Oriented Programming, and debugging methods. I became
-            president of DevLUp at UF because I wanted to inspire more computer
-            science students to work on projects that they are having fun doing,
-            and creating a game with Unity or Unreal can offer a lot to students
-            and improve their coding skills and literacy!
-          </p>
-          <p>
-            My experiences running a club and working in different fields have
-            taught me a lot about working as a team member and as a leader.
-            Throughout my early career as a software engineer, I have worked
-            with several different coding languages and code stacks, from
-            developing games to creating this website that you are currently
-            using! I have these projects detailed on my Project List page and
-            feel free to reach out to me!
+            In addition to my individual projects, I am the president of DevLUp,
+            a student club at the University of Florida dedicated to teaching
+            game development. Our mission is to help students learn key coding
+            principles such as GitHub, Object-Oriented Programming, and
+            debugging, while having fun with tools like Unity and Unreal Engine.
+            As its president, I aim to inspire fellow computer science students
+            to pursue projects they enjoy, which I believe enhances both their
+            skills and coding literacy. My leadership role and my work in
+            different technical environments have taught me the value of
+            teamwork, collaboration, and effective leadership.
           </p>
           <div className="object-container-horizon">
             <Link to="/projects" className="flex justify-center">
